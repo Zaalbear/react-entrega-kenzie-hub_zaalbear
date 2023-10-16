d770fc4e2e0bf94616cc155ef564d74072b1b7b3
João Barcellos <dev.zaalbear@gmail.com>
2023-10-16 16:25:36 -0300

    feat: feita a responsividade

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -18,9 +18,11 @@ export const Dashboard = ({ userData, setUserData }) => {
         <button className={styles.exit__bttn} onClick={exit}>Sair</button> 
       </div>
 
-      <div className={styles.banner__container}>
-        <h1 className={`${styles.banner__title} title1`}>Olá, {userData.name}</h1>
-        <p className={`${styles.banner__text} headlineBold`}>{userData.course_module}</p>
+      <div className={styles.banner__box}>
+        <div className={styles.banner__container}>
+          <h1 className={`${styles.banner__title} title1`}>Olá, {userData.name}</h1>
+          <p className={`${styles.banner__text} headlineBold`}>{userData.course_module}</p>
+        </div>
       </div>
 
       <div className={styles.main__container}>

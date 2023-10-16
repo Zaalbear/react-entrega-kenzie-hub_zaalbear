fce873f33582ab50fd1f79c537bb829a46d21991
João Barcellos <dev.zaalbear@gmail.com>
2023-10-16 16:28:46 -0300

    fix: adicionado reges de caractere especial

--- src/components/schemas/registerSchema/index.js
+++ src/components/schemas/registerSchema/index.js
@@ -8,7 +8,8 @@ export const registerSchema = z.object({
       .min(8, "É necessário pelo menos oito caracteres.")
       .regex(/(?=.*?[A-Z])/, "É necessário pelo menos uma letra maiúscula")
       .regex(/(?=.*?[a-z])/, "É necessário pelo menos uma letra minúscula")
-      .regex(/(?=.*?[0-9])/, "É necessário pelo menos um número."),
+      .regex(/(?=.*?[0-9])/, "É necessário pelo menos um número")
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, "É nescessário pelo menos um caractere especial"),
    confirmPassword: z.string().min(1, "É necessário confirmar a senha"),
    bio: z.string().min(1, "Esse campo é obrigatório"),
    contact: z.string().min(1, "Esse campo é obrigatório"),
